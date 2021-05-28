eed6faf26ebc5f5b49e6b76e049b9c6874da1983
Dylan Stankievech <dylan.stank@gmail.com>
2021-05-28 17:17:59 -0600

    Added Thing class, renamed things to repo, and implemented delete

--- servers/deno/src/routes.ts
+++ servers/deno/src/routes.ts
@@ -1,18 +1,16 @@
 import { RouterContext } from "./deps.ts";
-import thingsRepo from "./things.ts";
+import repo from "./repo.ts";
+import Thing from "./Thing.ts";
 
 export const getAllThings = async (ctx: RouterContext) => {
-  const things = await thingsRepo.getAll();
+  const things = await repo.getAll();
   ctx.response.body = things;
 };
 
 export const createThing = async (ctx: RouterContext) => {
   const { name, description } = await ctx.request.body().value;
-  const thing: any = {
-    name,
-    description,
-  };
-  const id = await thingsRepo.create(thing);
+  const thing = new Thing(name, description);
+  const id = await repo.create(thing);
   thing.id = id;
   ctx.response.status = 201;
   ctx.response.body = thing;
@@ -20,25 +18,30 @@ export const createThing = async (ctx: RouterContext) => {
 
 export const getThing = async (ctx: RouterContext) => {
   const id = parseInt(ctx.params.id ?? "", 10);
-  const thing: any = await thingsRepo.get(id);
+  const thing = await repo.get(id);
+  if (!thing) {
+    ctx.response.status = 404;
+    ctx.response.body = { message: "thing not found" };
+    return;
+  }
   ctx.response.body = thing;
 };
 
 export const updateThing = async (ctx: RouterContext) => {
   const id = parseInt(ctx.params.id ?? "", 10);
   const { name, description } = await ctx.request.body().value;
-  const thing = await thingsRepo.update(id, { name, description });
+  const thing = await repo.update(id, { name, description });
   if (!thing) {
     ctx.response.status = 404;
     ctx.response.body = { message: "thing not found" };
     return;
   }
-  ctx.response.body = await thingsRepo.get(id);
+  ctx.response.body = await repo.get(id);
 };
 
 export const deleteThing = async (ctx: RouterContext) => {
   const id = parseInt(ctx.params.id ?? "", 10);
-  const thing = await thingsRepo.delete(id);
+  const thing = await repo.delete(id);
 
   if (!thing) {
     ctx.response.status = 404;
