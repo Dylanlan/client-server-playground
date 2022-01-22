fce6ae47fe59bb86368158a0736047750dc1aa9d
Dylan Stankievech <dylan.stank@gmail.com>
2022-01-21 20:15:16 -0700

    Create a sample post page, with profile image

---
+++ servers/next/components/profile.tsx
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+
+export default function Profile() {
+  return <Image
+    src="/images/profile.png"
+    height={400}
+    width={310}
+    alt="Dylan Stankievech"
+  />
+}
---
+++ servers/next/pages/posts/first-post.tsx
@@ -0,0 +1,16 @@
+import Link from 'next/link'
+import Profile from '../../components/profile';
+
+export default function FirstPost() {
+  return (
+    <>
+      <h1>First Post</h1>
+      <h2>
+        <Link href="/">
+          <a>Back to home</a>
+        </Link>
+      </h2>
+      <Profile />
+    </>
+  )
+}
