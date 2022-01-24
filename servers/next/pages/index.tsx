b19cf99e57ab7367af1625c78f52e343786301dc
Dylan Stankievech <dylan.stank@gmail.com>
2022-01-23 18:38:00 -0700

    Initial markdown posts

--- servers/next/pages/index.tsx
+++ servers/next/pages/index.tsx
@@ -2,8 +2,15 @@ import Head from 'next/head'
 import Link from 'next/link';
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
+import { getSortedPostsData } from '../lib/posts'
 
-export default function Home() {
+type Post = {
+  id: string,
+  date: string,
+  title: string,
+};
+
+export default function Home({ allPostsData }: { allPostsData: Post[] }) {
   return (
     <Layout home>
       <Head>
@@ -14,12 +21,29 @@ export default function Home() {
           (This is a sample website)
         </p>
       </section>
-      <h1 className="title">
-        Read{' '}
-        <Link href="/posts/first-post">
-          <a>this page!</a>
-        </Link>
-      </h1>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
+      </section>
     </Layout>
   )
+}
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData
+    }
+  }
 }
