96d8cc974741d997aaeed16bf1979a47a10b24f8
Dylan Stankievech <dylan.stank@gmail.com>
2022-01-23 18:20:10 -0700

    Updated layout

--- servers/next/pages/posts/first-post.tsx
+++ servers/next/pages/posts/first-post.tsx
@@ -10,13 +10,12 @@ export default function FirstPost() {
         <Head>
           <title>First Post</title>
         </Head>
-        <h1>First Post</h1>
+        <h1>First Post...</h1>
         <h2>
           <Link href="/">
             <a>Back to home</a>
           </Link>
         </h2>
-        <Profile />
       </Layout>
     </>
   )
