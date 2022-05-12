e5fec74d43663aef3c8dfaf886343ffbc1c110f2
Dylan Stankievech <dylan.stank@gmail.com>
2022-05-12 17:50:46 -0600

    Add some more buttons for testing

--- clients/electron/buttons.js
+++ clients/electron/buttons.js
@@ -5,11 +5,55 @@ function setOnClicks() {
     if (refreshButton) {
         refreshButton.onclick = refreshPage;
     }
+
+    const exitButton = document.getElementById('exit-button');
+    if (exitButton) {
+        exitButton.onclick = exit;
+    }
+
+    const minimizeButton = document.getElementById('minimize-button');
+    if (minimizeButton) {
+        minimizeButton.onclick = minimize;
+    }
+
+    const maximizeButton = document.getElementById('maximize-button');
+    if (maximizeButton) {
+        maximizeButton.onclick = toggleMaximize;
+    }
 };
 
 function refreshPage() {
-    // TODO: refresh current page
-    //electron.remote.getCurrentWindow().reload();
+    const remote = electron.remote;
+    if (remote) {
+        remote.getCurrentWindow().reload();
+    }
+};
+
+function minimize() {
+    const remote = electron.remote;
+    if (remote) {
+        remote.getCurrentWindow().minimize();
+    }
+};
+
+function toggleMaximize(event) {
+    const remote = electron.remote;
+    const button = event.srcElement
+    if (remote && button) {
+        const window = remote.getCurrentWindow();
+        if (!window.isMaximized()) {
+            window.maximize();
+        } else {
+            window.unmaximize();
+        }
+    }
+};
+
+function exit() {
+    const remote = electron.remote;
+    if (remote) {
+        remote.getCurrentWindow().close();
+    }
 };
 
 module.exports = {
